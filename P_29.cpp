6e04099b86963f88cb3f9f2f7ee5287c7a06041f
Ritik Sharma <ritiksharma.14@gmail.com>
2025-08-19 08:03:51 +0530

    p29

---
+++ P_29.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+
+        for (auto &i : v)
+            cin >> i;
+        bool flag = false;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (v[i] < v[i + 1])
+            {
+                for (int j = i+2; j < n ; j++)
+                { 
+                    if( v[i+1] > v[j]){
+                        cout << "YES" << endl;
+                        cout << i << " " << i+1 << " " << j << endl; 
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            if(flag) break;
+        }
+        if(flag) continue;
+
+        cout << "NO" << endl;
+
+    }
+
+    return 0;
+}
